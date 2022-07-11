0c85e2b48b17d2dd785743a83c61b4d2b811548c
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2022-07-12 01:37:26 +0530

    Add files via upload

---
+++ DAY_5/Find_the_middle_of_LinkedList.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL)
+            return head;
+        ListNode *prev, *curr, *ans;
+        prev = head;
+        curr = head;
+        while(curr->next!=NULL)
+        {
+            prev = prev->next;
+            if(curr->next->next==NULL)
+                break;
+            curr = curr->next->next;
+        }
+        return prev;
+        
+    }
+};
